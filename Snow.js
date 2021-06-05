0dad38ef56ffeced11c513cb3cdf58da49eb3691
themathboi101 <78664310+themathboi101@users.noreply.github.com>
2021-06-05 19:28:37 -0400

    fixed snow.display

--- Snow.js
+++ Snow.js
@@ -3,10 +3,23 @@ class Snow{
         var options={
             density:1,
             friction:1,
-            isStatic=false,
+            isStatic:false,
         }
         this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("snow4.webp");
+    }
        
+        display(){
+         var snowPos=this.body.position;
+         push();
+         translate(pos.x,pos.y);
+         rectMode(CENTER);
+         fill("white");
+         rect(0,0,this.width,this.height);
+        // this.body.addImage(this.image);
+         pop();
+       }
     }
     
-}
